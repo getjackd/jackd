e58cfb7fcab2fb7105df3ecc9787a04b5f91f74f
Dexter <me@dextermiguel.com>
2019-03-06 12:53:16 -0800

    Formatting changes

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,50 +2,53 @@
 // Project: jackd
 // Definitions by: Tomasz Ciborski <https://tomasz.ciborski.com/>
 
-export = JackdClient;
+export = JackdClient
 
 declare class JackdClient {
-    constructor();
+  constructor()
 
-    connect(options: JackdClient.ConnectOptions): Promise<JackdClient>;
-    disconnect(): Promise<void>;
+  connect(options: JackdClient.ConnectOptions): Promise<JackdClient>
+  disconnect(): Promise<void>
 
-    put(stringOrObject: string | object, options?: JackdClient.PutOptions): Promise<void>;
-    reserve(): Promise<JackdClient.Job>;
-    delete(jobId: string): Promise<void>;
-    release(jobId: string, options?: JackdClient.ReleaseOptions): Promise<void>;
-    bury(jobId: string): Promise<void>;
-    kickJob(jobId: string): Promise<void>;
-    kick(jobsCount: number): Promise<void>;
-    touch(jobId: string): Promise<void>;
+  put(
+    stringOrObject: string | object,
+    options?: JackdClient.PutOptions
+  ): Promise<void>
+  reserve(): Promise<JackdClient.Job>
+  delete(jobId: string): Promise<void>
+  release(jobId: string, options?: JackdClient.ReleaseOptions): Promise<void>
+  bury(jobId: string): Promise<void>
+  kickJob(jobId: string): Promise<void>
+  kick(jobsCount: number): Promise<void>
+  touch(jobId: string): Promise<void>
 
-    use(tubeId: string): Promise<void>;
-    watch(tubeId: string): Promise<number>;
-    ignore(tubeId: string): Promise<number>;
+  use(tubeId: string): Promise<void>
+  watch(tubeId: string): Promise<number>
+  ignore(tubeId: string): Promise<number>
 
-    executeMultiPartCommand(command: string): Promise<string>;
-    executeCommand(...args: any): Promise<any>;
+  executeMultiPartCommand(command: string): Promise<string>
+  executeCommand(...args: any): Promise<any>
 }
 
 declare namespace JackdClient {
-    export interface ConnectOptions {
-        host: string;
-        port?: number;
-    }
+  export interface ConnectOptions {
+    host: string
+    port?: number
+  }
 
-    export interface PutOptions {
-        delay: number;
-        priority: number;
-        ttr: number;
-    }
+  export interface PutOptions {
+    delay: number
+    priority: number
+    ttr: number
+  }
 
-    export interface Job {
-        id: string;
-        payload: string;
-    }
+  export interface Job {
+    id: string
+    payload: string
+  }
 
-    export interface ReleaseOptions {
-        priority: number;
-        delay: number;
-    }
+  export interface ReleaseOptions {
+    priority: number
+    delay: number
+  }
 }
